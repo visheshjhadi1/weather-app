e72846357cfb6caf338d99c1432affafa493976f
Vishesh jhadi <visheshjhadi29@gmail.com>
2023-05-08 14:50:29 +0530

    header styling

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import {useEffect , useState} from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [Location , setLocation] = useState("");
+  
+
+
+
+
+
   return (
     <main>
       <section className="Content-section">
