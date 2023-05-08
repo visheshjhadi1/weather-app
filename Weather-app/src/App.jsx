45f08d0b8fe620b160f72bcf410ba234249671d8
Vishesh jhadi <visheshjhadi29@gmail.com>
2023-05-08 17:05:41 +0530

    Added Weather API and updated the fetch function

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -5,17 +5,34 @@ import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [Location , setLocation] = useState("");
+  const [Location , setLocation] = useState("Mumbai");
+  const [Current , setCurrent] = useState();
+
+  //API key = a907bc34abd44f91b1b104432230805
+  //q= (location name , or cordinates)
+//https://api.weatherapi.com/v1/forecast.json?key=a907bc34abd44f91b1b104432230805&q=London
+
+useEffect(()=>{
+
+  const key = 'a907bc34abd44f91b1b104432230805';
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${Location}`;
   
+  fetch(url)
+  .then (response => response.json())
+  .then(data => console.log(data))
 
+},[Location])
 
+const chooseLocation = (selectLoc) => {
+  setLocation(selectLoc);
+};
 
 
 
   return (
     <main>
       <section className="Content-section">
-        <Header />
+        <Header chooseLocation={chooseLocation} />
         <Main />
       </section>
       <section className="Content-Sidebar">
--- Weather-app/src/components/Header.jsx
+++ Weather-app/src/components/Header.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import logo from "../assets/logo.svg";
 
-export default function Header() {
+export default function Header({ chooseLocation }) {
+
+const [SearchedLocation , setSearchedLocation] = useState("");
+
+const handleChange = (e) => {
+    setSearchedLocation(e.target.value)
+}
+
+
+
   return (
     <div className="Main-header">
       <img src={logo} alt="App logo" />
       <div className="search-container">
-        <input type="text" placeholder="Search location here" className="searchBar"/>
-        <button className="SearchBtn" id="SearchBtn">Search</button>
+        <input type="text" placeholder="Search location here" className="searchBar" onChange={handleChange}/>
+        <button className="SearchBtn" id="SearchBtn" onClick={() => chooseLocation(SearchedLocation)}>Search</button>
       </div>
     </div>
   );
